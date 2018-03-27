770c402ef03bcfd94fd168348d1cd60251c9686d
Ty Eggen <tycho.eggen@gmail.com>
2018-03-26 22:06:11 -0700

    Restart always

--- do_docker.sh
+++ do_docker.sh
@@ -9,4 +9,4 @@ docker rmi deletron3030
 docker build -t deletron3030 .
 docker kill deletron3030
 docker rm deletron3030
-docker run -d --name deletron3030 deletron3030
+docker run -d --restart=always --name deletron3030 deletron3030
